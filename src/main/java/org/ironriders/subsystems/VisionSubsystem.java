3b6e843ec091dd110148a655084ea33d315502d4
ampersanded <salvobonsma@gmail.com>
2024-01-31 19:56:45 -0800

    Vision target logging fixed

--- src/main/java/org/ironriders/subsystems/VisionSubsystem.java
+++ src/main/java/org/ironriders/subsystems/VisionSubsystem.java
@@ -2,6 +2,7 @@ package org.ironriders.subsystems;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructArrayPublisher;
@@ -23,9 +24,14 @@ public class VisionSubsystem extends SubsystemBase {
     private final PhotonCamera camera = new PhotonCamera(VISION_CAMERA);
     private final PhotonPoseEstimator estimator;
     private final AprilTagFieldLayout aprilTagLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
-    private final StructArrayPublisher<Pose3d> visibleAprilTags = NetworkTableInstance
+    private final StructArrayPublisher<Pose3d> visibleAprilTags3D = NetworkTableInstance
             .getDefault()
             .getStructArrayTopic(DASHBOARD_PREFIX + "visibleAprilTags", Pose3d.struct)
+            .publish();
+
+    private final StructArrayPublisher<Pose2d> visibleAprilTags2D = NetworkTableInstance
+            .getDefault()
+            .getStructArrayTopic("vision/visibleAprilTags", Pose2d.struct)
             .publish();
     private boolean useVisionForEstimation = false;
 
@@ -40,12 +46,15 @@ public class VisionSubsystem extends SubsystemBase {
     public void periodic() {
         if (!RobotBase.isSimulation()) {
             int[] ids = getResult().getTargets().stream().mapToInt(PhotonTrackedTarget::getFiducialId).toArray();
-            Pose3d[] poses = new Pose3d[ids.length];
+            Pose3d[] poses3D = new Pose3d[ids.length];
+            Pose2d[] poses2D = new Pose2d[ids.length];
             for (int i = 0; i < ids.length; i++) {
-                poses[i] = aprilTagLayout.getTagPose(ids[i]).orElse(new Pose3d());
+                poses3D[i] = aprilTagLayout.getTagPose(ids[i]).orElse(new Pose3d());
+                poses2D[i] = poses3D[i].toPose2d();
             }
 
-            visibleAprilTags.set(poses);
+            visibleAprilTags3D.set(poses3D);
+            visibleAprilTags2D.set(poses2D);
         }
     }
 
