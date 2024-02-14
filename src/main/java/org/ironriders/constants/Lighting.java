c0a2f7a17270282e3eaa9ff14a0cd9a8823af083
ampersanded <salvobonsma@gmail.com>
2024-02-13 21:38:28 -0800

    Lights refactoring

--- src/main/java/org/ironriders/constants/Lighting.java
+++ src/main/java/org/ironriders/constants/Lighting.java
@@ -3,12 +3,13 @@ package org.ironriders.constants;
 
 
 public class Lighting {
-    public static final int RED = 0;
-    public static final int GREEN = 255;
-    public static final int BLUE = 0;
-    public static final int LIGHTCHANNELRED = 1;
-    public static final int LIGHTCHANNELGREEN=  2;
-    public static final int LIGHTCHANNELBLUE = 3;
-    public static final double RGBTOPULSEWIDTH = 16.05882352941176; // 4095/255 rgb values go from 0 to 295 while the pulse width 0 to 4095
+    public static final String DASHBOARD_PREFIX = "lights/";
+    // 4095/255 RGB values go from 0 to 295 while the pulse width 0 to 4095
+    public static final double PULSE_WIDTH = 4095 / 255.0;
 
+    public static class Color {
+        public static final int R = 0;
+        public static final int G = 255;
+        public static final int B = 0;
+    }
 }
--- src/main/java/org/ironriders/subsystems/LightingSubsystem.java
+++ src/main/java/org/ironriders/subsystems/LightingSubsystem.java
@@ -2,29 +2,30 @@ package org.ironriders.subsystems;
 
 
 import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import static org.ironriders.constants.Lighting.*;
-
+import static org.ironriders.constants.Lighting.Color.*;
+import static org.ironriders.constants.Lighting.DASHBOARD_PREFIX;
+import static org.ironriders.constants.Lighting.PULSE_WIDTH;
 
 public class LightingSubsystem extends SubsystemBase {
 
-    public PWM redChannel = new PWM(LIGHTCHANNELRED, false);
-    public PWM greenChannel = new PWM(LIGHTCHANNELGREEN, false);
-    public PWM blueChannel = new PWM(LIGHTCHANNELBLUE, false);
+    public PWM redChannel = new PWM(1, false);
+    public PWM greenChannel = new PWM(2, false);
+    public PWM blueChannel = new PWM(3, false);
 
     public LightingSubsystem() {
-        setColorRGB(RED,GREEN,BLUE);
-    }
+        SmartDashboard.putData(DASHBOARD_PREFIX + "redChannel", redChannel);
+        SmartDashboard.putData(DASHBOARD_PREFIX + "greenChannel", greenChannel);
+        SmartDashboard.putData(DASHBOARD_PREFIX + "blueChannel", blueChannel);
 
+        setColorRGB(R, G, B);
+    }
 
     public void setColorRGB(int r, int g, int b) {
-        redChannel.setPulseTimeMicroseconds((int) Math.round(r*RGBTOPULSEWIDTH));
-        greenChannel.setPulseTimeMicroseconds((int) Math.round(g*RGBTOPULSEWIDTH));
-        blueChannel.setPulseTimeMicroseconds((int) Math.round(b*RGBTOPULSEWIDTH));
-
+        redChannel.setPulseTimeMicroseconds((int) Math.round(r * PULSE_WIDTH));
+        greenChannel.setPulseTimeMicroseconds((int) Math.round(g * PULSE_WIDTH));
+        blueChannel.setPulseTimeMicroseconds((int) Math.round(b * PULSE_WIDTH));
     }
-
-
-
 }
