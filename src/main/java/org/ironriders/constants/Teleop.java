78b808ee8e9123ec057f486e731df6a5d88079d5
ampersanded <salvobonsma@gmail.com>
2024-03-06 15:28:51 -0800

    auto stuff

--- src/main/java/org/ironriders/constants/Teleop.java
+++ src/main/java/org/ironriders/constants/Teleop.java
@@ -4,7 +4,7 @@ public class Teleop {
     public static class Controllers {
         public static class Joystick {
             public static final double EXPONENT = 3;
-            public static final double DEADBAND = 0.15;
+            public static final double DEADBAND = 0.5;
         }
     }
 }
