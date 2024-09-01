e7df0bb675a04d1b79830a4e736cbd7b51e2967e
a-mishmash <83683166+a-mishmash@users.noreply.github.com>
2024-08-31 17:00:35 -0700

    Fixed a lil mistake

--- src/main/java/org/ironriders/constants/Drive.java
+++ src/main/java/org/ironriders/constants/Drive.java
@@ -8,11 +8,7 @@ import swervelib.math.SwerveMath;
 public class Drive {
     public static final String SWERVE_CONFIG_LOCATION = "swerve";
     public static final String DASHBOARD_PREFIX = "drive/";
-<<<<<<< HEAD
-    public static final double MAX_SPEED = 7;
-=======
     public static final double MAX_SPEED = 6.0; // We can figure this out later
->>>>>>> dce9e8c (Updated photonlib, added kraken support, and changed rotation control)
     public static final double CLIMBING_MODE_SPEED = 0.5;
 
     public enum Heading {
